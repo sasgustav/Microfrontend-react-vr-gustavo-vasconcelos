77542b0e5b4eb3881fddf98c568dc57a7cd05168
Gustavo Vasconcelos <gustavovasconcelos.sc@gmail.com>
2024-04-22 14:05:14 -0300

    fgh

--- app-cards/src/components/Modal/Modal.js
+++ app-cards/src/components/Modal/Modal.js
@@ -43,7 +43,7 @@ const Modal = ({ items, onClose }) => {
           ))}
         </ul>
         <div className="modal-actions">
-          <button className="modal-action-cancel" onClick={onClose}>Cancelar</button>
+          <button className="modal-action-cancel" onClick={onClose}>CANCELAR</button>
           <button className="modal-action" onClick={handlePurchase}>
             Concluir compras
           </button>
